9f4bdb8848b94a8f8dafbb575fa3daf3a5ba3028
Chris van Dal <cvandal@users.noreply.github.com>
2021-08-08 20:49:44 +1000

    get and set given and family names

---
+++ src/Lyfe/Data/Migrations/20210808044127_AddGivenAndFamilyNamesToUserModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lyfe.Data.Migrations
+{
+    public partial class AddGivenAndFamilyNamesToUserModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FamilyName",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "GivenName",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FamilyName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "GivenName",
+                table: "Users");
+        }
+    }
+}
--- src/Lyfe/Models/User.cs
+++ src/Lyfe/Models/User.cs
@@ -9,6 +9,9 @@ namespace Lyfe.Models
         [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
         public string Id { get; set; }
 
+        public string GivenName { get; set; }
+        public string FamilyName { get; set; }
+
         public ICollection<Exercise> Exercises { get; set; }
         public ICollection<Weight> Weights { get; set; }
     }
